81f5a373c1bab8aed2f2c58d3794359097831caa
Patrick-Medley <med21006@byui.edu>
2021-07-08 02:30:53 -0500

    Update

--- lesson-9/js/towns.js
+++ lesson-9/js/towns.js
@@ -13,21 +13,22 @@ fetch(requestURL)
         for (let i = 0; i < towns.length; i++ )
         {
 
-            let stat = document.createElement('section');
-            let h3 = document.createElement('h3');
-            h3.textContent = towns[i].name;
-            stat.appendChild(h3);
-            document.querySelector('div.stats').appendChild(stat);
-            let m = document.createElement('p');
-            m.textContent = towns[i].motto;
-            stat.appendChild(m);
-            let yf = document.createElement('p');
-            yf.textContent = "Year Founded: " +towns[i].yearFounded;
-            stat.appendChild(yf);
-            let image= document.createElement('img')
-            image.setAttribute('src', towns[i].photo);
-            stat.appendChild(image)
-
+          let stat = document.createElement('section');
+          let art = document.createElement('article');
+          stat.appendChild(art);
+          document.querySelector('div.stats').appendChild(stat);
+          let h3 = document.createElement('h3');
+          h3.textContent = towns[i].name;
+          art.appendChild(h3);
+          let m = document.createElement('p');
+          m.textContent = towns[i].motto;
+          art.appendChild(m);
+          let yf = document.createElement('p');
+          yf.textContent = "Year Founded: " +towns[i].yearFounded;
+          art.appendChild(yf);
+          let image= document.createElement('img')
+          image.setAttribute('src', towns[i].photo);
+          stat.appendChild(image);
         }
 
     });
